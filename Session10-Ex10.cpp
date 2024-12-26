a7b86cb07e71984866e6db06426532072160bda5
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-12-26 20:01:31 +0700

    Add files via upload

---
+++ Session10-Ex10.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    int matrix[100][100], rows, cols;
+    printf("Nhap so dong: ");
+    scanf("%d", &rows);
+    printf("Nhap so cot: ");
+    scanf("%d", &cols);
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Phan tu [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for (int i = 0; i < rows - 1 && i < cols - 1; i++) {
+        for (int j = i + 1; j < rows && j < cols; j++) {
+            if (matrix[i][cols - i - 1] < matrix[j][cols - j - 1]) {
+                int temp = matrix[i][cols - i - 1];
+                matrix[i][cols - i - 1] = matrix[j][cols - j - 1];
+                matrix[j][cols - j - 1] = temp;
+            }
+        }
+    }
+    printf("Ma tran sau khi sap xep cac phan tu tren duong cheo phu giam dan:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
